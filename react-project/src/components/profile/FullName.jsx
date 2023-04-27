357275cb52fc78d28c3cff4004af901e01f64bc3
faraowait <arafabubakar4@gmail.com>
2023-04-27 06:49:32 +0100

    jsx

---
+++ react-project/src/components/profile/FullName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <div>
+        <h1>Abubakar Araf</h1>
+    </div>
+  )
+}
+
+export default FullName
---
+++ react-project/src/components/profile/ProfilePhoto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+        <img src={{}} alt=''/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
